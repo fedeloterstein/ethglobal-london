6a076ba0c399db6966a5130d6ca549918279967d
Fedev <federicoloterstein@gmail.com>
2024-03-16 14:07:28 +0000

    refactor

--- packages/react-app/components/views/Benefits/BenefitsCard.tsx
+++ packages/react-app/components/views/Benefits/BenefitsCard.tsx
@@ -38,7 +38,7 @@ export const BenefitsCard = ({ imageOne, imageTwo, title }: any) => {
         </Stack>
       </HStack>
       <Text>
-        Lorem Ipsum has been the industry's standard dummy text ever since the
+        Lorem Ipsum has been the industrys standard dummy text ever since the
         1500s, when an unknown printer took a galley of type and scrambled it to
         make a type specimen book.
       </Text>
